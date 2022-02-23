270e6b3bd619615950833da508ecaba3e307e9cb
vaishnavi <vaishnavichandrasekar123@gmail.com>
2022-02-23 22:18:13 +0530

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -53,8 +53,8 @@ class Home extends Component {
               duration={100}
             >Contact</Link>
 
-<a href="https://docs.google.com/document/d/1gWuOi1F3q193Ucp5JdZgfqtFk1-6AwOw/edit"
-              download="https://docs.google.com/document/d/1gWuOi1F3q193Ucp5JdZgfqtFk1-6AwOw/edit" target="_blank">
+<a href="https://docs.google.com/document/d/1_z0XapHQpyKKSncrkxaa6f-RF2uNBucc/edit?usp=drivesdk&ouid=104597042631528438052&rtpof=true&sd=true"
+              download="https://docs.google.com/document/d/1_z0XapHQpyKKSncrkxaa6f-RF2uNBucc/edit?usp=drivesdk&ouid=104597042631528438052&rtpof=true&sd=true" target="_blank">
               <button type="Submit" id='cvbtn'>Resume</button>
             </a>
 
